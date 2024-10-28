2698ea23122cb0e24c68423f6e87918034937e62
vorgestern <vorgestern@niemals>
2024-10-28 23:37:37 +0100

    Example a2: Verbesserung/Lesbarkeit

--- examples/a2.cpp
+++ examples/a2.cpp
@@ -7,14 +7,13 @@ using namespace std;
 int democlosure(lua_State*L)
 {
     LuaStack Q(L);
-    Q<<LuaTable(0, 0)<<LuaUpValue(1)<<luarot_3;              // Result, Map, Arg
+    Q<<LuaArray(0)<<LuaUpValue(1)<<luarot_3;                 // Result, Map, Arg
     auto Result=Q.index(-3), Map=Q.index(-2);
     for (LuaIterator J(Q); next(J); ++J)                     // Result, Map, Arg, key, value
     {
         Q.dup(-1);
         lua_gettable(L, stackindex(Map));                    // Result, Map, Arg, key, value, Map[value]
-        // Wenn nil: Result[J]=value
-        // sonst:    Result[J]=Map[value]
+        // Result[J]=Map[value] or value
         if (Q.hasnilat(-1)) Q.drop(1).dup(-1);               // Result, Map, Arg, key, value, value
         lua_seti(L, stackindex(Result), (unsigned)J);        // Result, Map, Arg, key, value
     }                                                        // Result, Map, Arg
@@ -29,6 +28,7 @@ int main()
     if (true)
     {
         vector<string>Arg {"Hier", "wohnen", "die", "Schlümpfe"};
+        Q<<Arg;
         // Make Map, Make Closure with upvalue==Map, Call Closure with Argument table
         Q   <<unordered_map<string,string> {{"Hier", "Dort"}, {"wohnen", "arbeiten"}, {"Schlümpfe", "Zwerge"}}<<LuaClosure(democlosure, 1)
             <<Arg>>1;
