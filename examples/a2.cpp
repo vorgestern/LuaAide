96e25f034513c65992f5ae78074c169fa9040e1c
vorgestern <vorgestern@niemals>
2024-10-29 18:13:58 +0100

    Nachtrag

--- examples/a2.cpp
+++ examples/a2.cpp
@@ -29,12 +29,35 @@ int main()
     {
         vector<string>Arg {"Hier", "wohnen", "die", "Schlümpfe"};
         Q<<Arg;
-        // Make Map, Make Closure with upvalue==Map, Call Closure with Argument table
+        // Make Map, Make Closure with upvalue==Map, Call Closure with Argument Arg.
         Q   <<unordered_map<string,string> {{"Hier", "Dort"}, {"wohnen", "arbeiten"}, {"Schlümpfe", "Zwerge"}}<<LuaClosure(democlosure, 1)
             <<Arg>>1;
         Q   <<unordered_map<string,string> {{"Hier", "Wo"}, {"wohnen", "schlafen"}, {"Schlümpfe", "Heinzelmännchen"}}<<LuaClosure(democlosure, 1)
             <<Arg>>1;
         cout<<"\n"<<Q<<"\n";
+    }
+    if (true)
+    {
+        Q<<LuaCode(R"xxx(
+            function translate(A, M)
+                local R={}
+                for _,e in ipairs(A) do table.insert(R, M[e] or e) end
+                return R
+            end
+            Closures={
+                fleissig=function(A) return translate(A, {Hier="Dort", wohnen="arbeiten", ["Schlümpfe"]="Zwerge"}) end,
+                faul=    function(A) return translate(A, {Hier="Wo", wohnen="schlafen", ["Schlümpfe"]="Heinzelmännchen"}) end
+            }
+        )xxx")>>0;
+        Q<<LuaGlobal("Closures")<<LuaDotCall("fleissig")<<vector<string>{"Hier", "wohnen", "die", "Schlümpfe"}>>1;
+        Q<<LuaGlobal("table")<<LuaDotCall("concat")<<LuaValue(-2)<<" ">>1;
+        Q<<luaswap; Q.drop(1);
+        Q<<LuaGlobalCall("print")<<LuaValue(-2)>>0;
+
+        Q<<LuaGlobal("Closures")<<LuaDotCall("faul")<<vector<string>{"Hier", "wohnen", "die", "Schlümpfe"}>>1;
+        Q<<LuaGlobal("table")<<LuaDotCall("concat")<<LuaValue(-2)<<" ">>1;
+        Q<<luaswap; Q.drop(1);
+        Q<<LuaGlobalCall("print")<<LuaValue(-2)>>0;
     }
     return 0;
 }
