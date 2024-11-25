9cc586413dd26cae401748f4a195e9f5c12ecbd1
vorgestern <fisches@gmx.de>
2024-11-25 01:02:35 +0100

    ulutest: Aufgeräumt

--- buildsys/VS17/ulutest_main.cpp
+++ buildsys/VS17/ulutest_main.cpp
@@ -10,16 +10,16 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     {
         case DLL_PROCESS_ATTACH:
             ulutest_module=hModule;
-            printf("DllMain PROCESS_ATTACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
+            // printf("DllMain PROCESS_ATTACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
             break;
         case DLL_THREAD_ATTACH:
-            printf("DllMain THREAD_ATTACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
+            // printf("DllMain THREAD_ATTACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
             break;
         case DLL_THREAD_DETACH:
-            printf("DllMain THREAD_DETACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
+            // printf("DllMain THREAD_DETACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
             break;
         case DLL_PROCESS_DETACH:
-            printf("DllMain PROCESS_DETACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
+            // printf("DllMain PROCESS_DETACH: (%p, %u, %p)\n", hModule, ul_reason_for_call, lpReserved);
             break;
     }
     return TRUE;
