73b41b6ff3b8aaf940dd0d0838730617e56dd52d
vorgestern <fisches@gmx.de>
2024-11-30 18:04:56 +0100

    VS17/Tester: Verbesserung
    - compile/make works as expected.
    - TestSummary handles missing inputs.

--- TestSummary.lua
+++ TestSummary.lua
@@ -1,13 +1,17 @@
 
 local summary={
-    {name='Alltagstest.result', passed=16, failed=0, failedtests={}},
-    {name='LuaAideTest.result', passed=47, failed=0, failedtests={}},
-    {name='m1test.result', passed=11, failed=0, failedtests={}},
-    {name='m2test.result', passed=7, failed=0, failedtests={}}
+    {name='Alltagstest', passed=16, failed=0, failedtests={}},
+    {name='LuaAideTest', passed=47, failed=0, failedtests={}},
+    {name='m1test', passed=11, failed=0, failedtests={}},
+    {name='m2test', passed=7, failed=0, failedtests={}}
 }
 
     print(string.format("%2s: %4s|%-4s %s (%s)", "#", "ok", "fail", "testname", "failed tests"))
     for j,r in ipairs(summary) do
-        local F=table.concat(r.failedtests, ", ")
-        print(string.format("%2d: %4d|%-4d %s (%s)", j, r.passed, r.failed, r.name, F))
+        if r.filenotfound then
+            print(string.format("%2d: %4s|%-4s %s (file not found: '%s')", j, "-", "-", r.name, r.filenotfound))
+        else
+            local F=table.concat(r.failedtests, ", ")
+            print(string.format("%2d: %4d|%-4d %s (%s)", j, r.passed, r.failed, r.name, F))
+        end
     end
--- buildsys/generic/summarise_tests.lua
+++ buildsys/generic/summarise_tests.lua
@@ -2,11 +2,15 @@
 local function resformat(S)
     local X={}
     for _,A in ipairs(S) do
-        local Lf=""
-        if #A.failedtests>0 then
-            Lf="'"..table.concat(A.failedtests, "', '").."'"
+        if A.filenotfound then
+            table.insert(X, string.format("{name='%s', filenotfound='%s'}", A.name, A.filenotfound))
+        else
+            local Lf=""
+            if #A.failedtests>0 then
+                Lf="'"..table.concat(A.failedtests, "', '").."'"
+            end
+            table.insert(X, string.format("{name='%s', passed=%d, failed=%d, failedtests={%s}}", A.name, A.passed, A.failed, Lf))
         end
-        table.insert(X, string.format("{name='%s', passed=%d, failed=%d, failedtests={%s}}", A.name, A.passed, A.failed, Lf))
     end
     return "local summary={\n    "..table.concat(X, ",\n    ").."\n}"
 end
@@ -14,17 +18,23 @@ end
 local function main(ziel, args)
     local S={}
     for _,f in ipairs(args) do
-        local A={name=f, failedtests={}, passed=0, failed=0}
+        local A={name=f:match "(.*)%.result", failedtests={}, passed=0, failed=0, filenotfound=nil}
         table.insert(S, A)
-        for line in io.lines(f) do
-            -- print("==", line)
-            local num=line:match "%[  PASSED  %]%s+(%d+)%s+tests"
-            if num then A.passed=num end
-            local num=line:match "%[  FAILED  %]%s+(%d+)%s+test"
-            if num then A.failed=tonumber(num)
-            elseif A.failed>0 then
-                local name=line:match "%[  FAILED  %]%s+(.+)"
-                if name then table.insert(A.failedtests, name) end
+        local checkfile=io.open(f, "r")
+        if not checkfile then
+            A.filenotfound=f
+        else
+            checkfile:close()
+            for line in io.lines(f) do
+                -- print("==", line)
+                local num=line:match "%[  PASSED  %]%s+(%d+)%s+tests"
+                if num then A.passed=num end
+                local num=line:match "%[  FAILED  %]%s+(%d+)%s+test"
+                if num then A.failed=tonumber(num)
+                elseif A.failed>0 then
+                    local name=line:match "%[  FAILED  %]%s+(.+)"
+                    if name then table.insert(A.failedtests, name) end
+                end
             end
         end
     end
@@ -32,8 +42,12 @@ local function main(ziel, args)
     io.output(ziel):write("\n", T, "\n\n", [[
     print(string.format("%2s: %4s|%-4s %s (%s)", "#", "ok", "fail", "testname", "failed tests"))
     for j,r in ipairs(summary) do
-        local F=table.concat(r.failedtests, ", ")
-        print(string.format("%2d: %4d|%-4d %s (%s)", j, r.passed, r.failed, r.name, F))
+        if r.filenotfound then
+            print(string.format("%2d: %4s|%-4s %s (file not found: '%s')", j, "-", "-", r.name, r.filenotfound))
+        else
+            local F=table.concat(r.failedtests, ", ")
+            print(string.format("%2d: %4d|%-4d %s (%s)", j, r.passed, r.failed, r.name, F))
+        end
     end
 ]])
     io.close()
