63f6d4b39bcdf4d1fd2acf508efe39a69945402d
vorgestern <fisches@gmx.de>
2026-09-04 02:48:23 +0200

    Simple Unittest for map

--- src/map.cpp
+++ src/map.cpp
@@ -26,3 +26,39 @@ int LuaAide::map(lua_State*L)
     Q<<mappedlist;
     return 1;
 }
+
+// ============================================================================
+
+#ifdef UNITTEST
+#include <gtest/gtest.h>
+
+static int panichandler(lua_State*L)
+{
+    LuaStack Q(L);
+    throw runtime_error(Q.errormessage());
+    return 0;
+}
+
+class MapEnv: public ::testing::Test
+{
+protected:
+    LuaStack Q{};
+    void SetUp() override { Q=LuaStack::New(true, panichandler); }
+    void TearDown() override { Q.Close(); }
+};
+
+TEST_F(MapEnv, SimpleExample)
+{
+    Q<<LuaCode("return function(x) return 100+x end")>>1;
+    auto mapfunc=Q.index(-1);
+    Q<<lualist<<21<<22<<23<<lualistend;
+    Q<<LuaAide::map<<LuaValue(-2)<<mapfunc>>1;
+    ASSERT_EQ((int)LuaType::TTABLE, (int)Q.typeat(-1));
+    for (LuaIterator J(Q); next(J); ++J)
+    {
+        EXPECT_EQ(LuaType::TNUMBER, Q.typeat(-1));
+        EXPECT_EQ(20+(unsigned)J+100, Q.toint(-1));
+    }
+}
+
+#endif
