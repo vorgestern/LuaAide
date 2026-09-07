d9ec77be6a8228077693f90f8520a0599dcdd2ce
vorgestern <fisches@gmx.de>
2026-09-07 14:31:09 +0200

    Improved TestSummary

--- TestSummary.lua
+++ TestSummary.lua
@@ -19,8 +19,7 @@ local summary={
     print [[
 
 To execute tests directly:
-.\LuaAideTest
-lua examples\moduletest_vec3.lua
-lua examples\moduletest_timestamp.lua
-lua examples\moduletest_colorenum.lua]]
-    
+    .\LuaAideTest
+    lua examples\moduletest_vec3.lua
+    lua examples\moduletest_timestamp.lua
+    lua examples\moduletest_colorenum.lua]]
